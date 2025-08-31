a00add225e9799ca8177be109897e490c2382ae6
Hanan Elhariri <hanan.elhariri@yourparts.com>
2025-08-31 15:50:50 +0300

    init app

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Elysia } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+
+import { note } from './note'
+
+const app = new Elysia()
+    .use(swagger())
+    
+    .use(note) 
+
+    .listen(3000)
---
+++ src/note.ts
@@ -0,0 +1,64 @@
+import { Elysia, t } from 'elysia'
+
+class Note {
+    constructor(public data: string[] = ['Moonhalo']) { }
+
+    add(note: string) {
+        this.data.push(note)
+
+        return this.data
+    }
+
+    remove(index: number) {
+        return this.data.splice(index, 1)
+    }
+
+    update(index: number, note: string) {
+        return (this.data[index] = note)
+    }
+}
+
+export const note = new Elysia({ prefix: '/note' })
+    .decorate('note', new Note())
+
+    .get('/', ({ note }) => note.data)
+
+    .put('/', ({ note, body: { data } }) => note.add(data), {
+        body: t.Object({
+            data: t.String()
+        })
+    })
+
+    .guard({
+        params: t.Object({
+            index: t.Number()
+        })
+    })
+
+    .get(
+        '/:index',
+        ({ note, params: { index }, status }) => {
+            return note.data[index] ?? status(404, 'Not Found :(')
+        },
+    )
+    .delete(
+        '/:index',
+        ({ note, params: { index }, status }) => {
+            if (index in note.data) return note.remove(index)
+
+            return status(422)
+        },
+    )
+    .patch(
+        '/:index',
+        ({ note, params: { index }, body: { data }, status }) => {
+            if (index in note.data) return note.update(index, data)
+
+            return status(422)
+        },
+        {
+            body: t.Object({
+                data: t.String()
+            })
+        }
+    ) 
