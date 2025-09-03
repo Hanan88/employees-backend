dc8e1bd9878f017ea58934768d44abe39f7607a5
Hanan Elhariri <hanan.elhariri@yourparts.com>
2025-09-03 16:38:28 +0300

    chore: install cros

--- src/features/employees/controllers/EmployeesController.ts
+++ src/features/employees/controllers/EmployeesController.ts
@@ -1,10 +1,22 @@
 import { Elysia } from "elysia";
+import db from "../../../knexfile";
 
-import db from "../../../knexfile"; 
 
 export const employeesController = new Elysia({ prefix: "/employees" })
     // Get all employees
     .get("/", async () => {
         const employees = await db("employees").select("*");
         return employees;
+    })
+
+    //Get employee by id
+    .get("/:id", async ({ params: { id } }) => {
+        const employee = await db("employees").where("id", id).first();
+        return employee;
     });
+
+//Create employee
+// .post("/", async ({ body }) => {
+//     const employee = await db("employees").insert(body);
+//     return employee;
+// });
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import { Elysia } from 'elysia'
 import { swagger } from '@elysiajs/swagger'
 
 import EmployeesRoute from './features/employees/routes/EmployeesRoute'
+import { cors } from '@elysiajs/cors';
 
 const app = new Elysia()
+    .use(cors())
     .use(swagger())
-
     .use(EmployeesRoute)
 
     .listen(3000)
